a8359c7ffbcbaa2ad2f8b9b51d5a87facaa5414b
mtour2022 <mtour.it2022@gmail.com>
2025-06-18 13:18:41 +0800

    add new images

--- src/pages/EssentialsPage.jsx
+++ src/pages/EssentialsPage.jsx
@@ -57,50 +57,50 @@ export default function EssentialsPage() {
   const cardData = [
     {
       name: "EMERGENCY HOTLINES",
-      caption: "For emergencies and important inquiries.",
+      caption: "For emergencies and important inquiries",
       link: "/listview/hotlines",
       color: "#DA3C3C", // Green
-      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/attractions.png?alt=media&token=84f0cbf2-4986-4fa3-ae02-80a52319534f",
+      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/hotlines.jpg?alt=media&token=71fedd52-62b5-4ed1-b026-6b8bc976ccbd",
       icon: faPhone,
     },
     {
       name: "TOURIST REQUIREMENTS",
-      caption: "Important things to bring.",
+      caption: "Important things to bring",
       link: "/listview/requirements",
       color: "#2D8B3C", // Blue
-      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/activities.jpg?alt=media&token=bc2a2009-b794-47b6-8f36-93c9394e24aa",
+      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/touristrequirements.jpg?alt=media&token=4d2d0f39-9bbf-4526-bdee-49ac1b2ca9f8",
       icon: faPassport,
     },
     {
       name: "BEACH LAWS",
-      caption: "Ordinances to remember.",
+      caption: "Ordinances to remember",
       link: "/listview/ordinances",
       color: "#263238", // Blue
-      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/activities.jpg?alt=media&token=bc2a2009-b794-47b6-8f36-93c9394e24aa",
+      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/police.jpg?alt=media&token=d77dfea0-c94a-4cfc-ab0e-977b55625702",
       icon: faGavel,
     },
     {
-      name: "LIFESTYLE & FACILITIES",
-      caption: "Know more about the way of living.",
+      name: "LIFESTYLES & FACILITIES",
+      caption: "Know more about the way of living",
       link: "/update/lifeStyles",
       color: "#A051EE", // Blue
-      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/activities.jpg?alt=media&token=bc2a2009-b794-47b6-8f36-93c9394e24aa",
+      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/facilities.jpg?alt=media&token=31377dac-d19a-432b-b3d5-f1c10ec1a316",
       icon: faSun,
     },
     {
       name: "HELPFUL LINKS",
-      caption: "For a seemless travel.",
+      caption: "For a seemless travel",
       link: "/update/helpfulLinks",
       color: "#4682B4", // Blue
-      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/activities.jpg?alt=media&token=bc2a2009-b794-47b6-8f36-93c9394e24aa",
+      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/apps.png?alt=media&token=51eb06e4-d876-489b-9e31-2549c0d5afd7",
       icon: faGlobe,
     },
     {
       name: "TOURIST FAQS",
-      caption: "For a seemless travel.",
+      caption: "For a seemless travel",
       link: "/listview/touristFAQs",
       color: "#FF8A00", // Blue
-      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/activities.jpg?alt=media&token=bc2a2009-b794-47b6-8f36-93c9394e24aa",
+      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/information.jpg?alt=media&token=5d2dd5e0-3f9b-441f-b06d-76b1107042e7",
       icon: faQuestionCircle,
     },
   ];
@@ -108,7 +108,7 @@ export default function EssentialsPage() {
   const cardData2 = [
     {
       name: "TO SUPPORT",
-      caption: "Tourism campaigns to support.",
+      caption: "Tourism campaigns to support",
       link: "/slideshow/sustainableTourism",
       color: "#2E7D32", // Green
       image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/attractions.png?alt=media&token=84f0cbf2-4986-4fa3-ae02-80a52319534f",
@@ -116,7 +116,7 @@ export default function EssentialsPage() {
     },
     {
       name: "TO AVOID",
-      caption: "Some things to avoid.",
+      caption: "Some things to avoid",
       link: "/slideshow/sustainableTourism",
       color: "#B7410E", // Blue
       image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/activities.jpg?alt=media&token=bc2a2009-b794-47b6-8f36-93c9394e24aa",
@@ -124,7 +124,7 @@ export default function EssentialsPage() {
     },
     {
       name: "TO BRING",
-      caption: "Helpful things to save mother earth.",
+      caption: "Helpful things to save mother earth",
       link: "/slideshow/sustainableTourism",
       color: "#CD5C5C", // Blue
       image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/activities.jpg?alt=media&token=bc2a2009-b794-47b6-8f36-93c9394e24aa",
@@ -132,7 +132,7 @@ export default function EssentialsPage() {
     },
     {
       name: "TO REMEMBER",
-      caption: "Things to keep in mind.",
+      caption: "Things to keep in mind",
       link: "/slideshow/sustainableTourism",
       color: "#FBC02D", // Blue
       image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/activities.jpg?alt=media&token=bc2a2009-b794-47b6-8f36-93c9394e24aa",
@@ -143,11 +143,11 @@ export default function EssentialsPage() {
 
   const cardData3 = [
     {
-      name: "AWARDS & RECONITIONS",
-      caption: "Award-winning gallery.",
+      name: "AWARDS, RECONITIONS & CERTIFICATIONS",
+      caption: "Excellence You Can Trust",
       link: "/update/awardsAndRecognitions",
       color: "#D6C334", // Green
-      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/attractions.png?alt=media&token=84f0cbf2-4986-4fa3-ae02-80a52319534f",
+      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/awards-min.jpg?alt=media&token=4332a975-e379-4c40-8f8c-9f9807f771b2",
       icon: faMedal,
     },
     {
@@ -155,23 +155,23 @@ export default function EssentialsPage() {
       caption: "Expo & B2B Particiipations",
       link: "/update/travelExpos",
       color: "#8B4513", // Blue
-      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/activities.jpg?alt=media&token=bc2a2009-b794-47b6-8f36-93c9394e24aa",
+      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/expos-min.jpg?alt=media&token=2b8f0c43-0f2e-4326-b9a1-10ad4cb0fbe2",
       icon: faHandshake,
     },
     {
       name: "TOURISM PROJECTS",
-      caption: "Projects for the people in tourism.",
+      caption: "Projects for the people in tourism",
       link: "/update/tourismProjects",
       color: "#04D9A4", // Blue
-      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/activities.jpg?alt=media&token=bc2a2009-b794-47b6-8f36-93c9394e24aa",
+      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/projects-min.jpg?alt=media&token=5e0f89c6-f77d-4e3a-a512-bb759c6626f3",
       icon: faHandsHelping,
     },
     {
       name: "TOURISM NICHES",
-      caption: "Different people, various offerings.",
+      caption: "Different people, various offerings",
       link: "/update/tourismMarkets",
       color: "#004D40", // Blue
-      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/activities.jpg?alt=media&token=bc2a2009-b794-47b6-8f36-93c9394e24aa",
+      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/niche-min.jpg?alt=media&token=b8a4947e-5d24-4bb3-b2cb-298a0e43c361",
       icon: faMagnifyingGlass,
     },
   ];
@@ -179,10 +179,10 @@ export default function EssentialsPage() {
    const cardData4 = [
     {
       name: "CRUISE SHIP ARRIVAL",
-      caption: "Award-winning gallery.",
+      caption: "Award-winning gallery",
       link: "/update/cruiseShips",
       color: "#FFA500", // Green
-      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/attractions.png?alt=media&token=84f0cbf2-4986-4fa3-ae02-80a52319534f",
+      image: "https://firebasestorage.googleapis.com/v0/b/infoguide-13007.firebasestorage.app/o/cruiseship.jpg?alt=media&token=0be7c5da-914d-4080-b7e4-2b842d53fb6a",
       icon: faShip,
     },
    
